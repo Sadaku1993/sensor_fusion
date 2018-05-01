cbc450895da843918184ee36308618998bebd09e
Sadaku1993 <Sadaku1993@users.noreply.github.com>
2018-05-01 21:51:20 +0900

    add colouring code

--- src/coloring_zed0.cpp
+++ src/coloring_zed0.cpp
@@ -29,6 +29,8 @@ typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudXYZRGB;
 
 ros::Time t;
 
+ros::Publisher pub;
+
 string target_frame = "/zed0/zed_left_camera";
 string source_frame = "/centerlaser";
 
@@ -68,6 +70,49 @@ void colouring(sensor_msgs::PointCloud2 pc_msg, const sensor_msgs::CameraInfoCon
 {
     cout<<"ALL GREEN"<<endl;
 
+    cv_bridge::CvImageConstPtr cv_img_ptr;
+    try{
+      cv_img_ptr = cv_bridge::toCvShare(image_msg);
+    }catch (cv_bridge::Exception& e){
+      ROS_ERROR("cv_bridge exception: %s", e.what());
+      return;
+    }
+    cv::Mat image(cv_img_ptr->image.rows, cv_img_ptr->image.cols, cv_img_ptr->image.type());
+    image = cv_bridge::toCvShare(image_msg)->image;
+
+    image_geometry::PinholeCameraModel cam_model_;
+    cam_model_.fromCameraInfo(cinfo_msg);
+
+    pcl::PointCloud<pcl::PointXYZ>::Ptr trans_cloud(new pcl::PointCloud<pcl::PointXYZ>);
+    PointCloudXYZRGB::Ptr coloured = PointCloudXYZRGB::Ptr(new PointCloudXYZRGB);
+
+    fromROSMsg(pc_msg, *trans_cloud);
+
+    trans_cloud->header.frame_id = target_frame;
+
+    pcl::copyPointCloud(*trans_cloud, *coloured);
+    
+    for (pcl::PointCloud<pcl::PointXYZRGB>::iterator pt = coloured->points.begin(); pt < coloured->points.end(); pt++)
+    {
+        if ((*pt).x<0) continue;
+        cv::Point3d pt_cv(-(*pt).y, (*pt).z, (*pt).x);
+        cv::Point2d uv;
+        uv = cam_model_.project3dToPixel(pt_cv);
+
+        if(uv.x>0 && uv.x < image.cols && uv.y > 0 && uv.y < image.rows){
+            (*pt).b = image.at<cv::Vec3d>(uv)[0];
+            (*pt).g = image.at<cv::Vec3d>(uv)[1];
+            (*pt).r = image.at<cv::Vec3d>(uv)[2];
+        }
+    }
+
+    // Publish Coloured PointCloud
+    sensor_msgs::PointCloud2 pcl_coloured;
+    pcl::toROSMsg(*coloured, pcl_coloured);
+    pcl_coloured.header.frame_id = target_frame;
+    pcl_coloured.header.stamp = t;
+    pub.publish(pcl_coloured);
+
 }
 
 int main(int argc, char** argv)
@@ -80,6 +125,8 @@ int main(int argc, char** argv)
     ros::Subscriber pc_sub    = n.subscribe("/sq_lidar/points/center", 10, pcCallback); 
     ros::Subscriber cinfo_sub = n.subscribe("/zed0/left/camera_info", 10, cameraCallback);
     ros::Subscriber image_sub = n.subscribe("/zed0/left/image_rect_color", 10, imageCallback);
+
+    pub = n.advertise<sensor_msgs::PointCloud2>("/zed0/coloured_points", 10);
 
     ros::Rate rate(20);
 
