8d4abc2b1dd92b9124edbf225909a7e37cec578e
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-05-22 17:55:48 +0900

    add new code

---
+++ src/coloring.cpp
@@ -0,0 +1,20 @@
+#include <ros/ros.h>
+#include "ros/package.h"
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+#include <pcl/point_cloud.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/common/transforms.h>
+#include <pcl_ros/transforms.h>
+#include <opencv2/opencv.hpp>
+#include <cv_bridge/cv_bridge.h>
+#include <image_geometry/pinhole_camera_model.h>
+#include <sensor_msgs/CameraInfo.h>
+#include <sensor_msgs/PointCloud.h>
+#include <sensor_msgs/point_cloud_conversion.h>
+
+using namespace std;
+
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudXYZRGB;
+
+
