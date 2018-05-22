a07eab0a7adf58ee9ac143b11657aa9df788fd80
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-05-22 17:13:58 +0900

    correct code

--- test/test_points_callback.cpp
+++ test/test_points_callback.cpp
@@ -19,8 +19,8 @@ ros::Publisher pub;
 
 pc_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
-    sensor_msgs::PointCloud2ConstPtr pc;
-    pc = msg;
+    sensor_msgs::PointCloud2 pc;
+    pc = *msg;
     pub.publish(pc);
 }
 
