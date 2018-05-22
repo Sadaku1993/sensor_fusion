96c1918df3d5618024079b3d8a9f1f81126f4efd
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-05-22 17:04:54 +0900

    add comment

--- test/test_points_callback.cpp
+++ test/test_points_callback.cpp
@@ -1,23 +1,17 @@
+/*
+test code for subscribe pointcloud
+
+Subscribe:
+    PointCloud2
+Publish:
+    PointCloud2
+
+author:Yudai Sadakuni
+
+*/
+
 #include <ros/ros.h>
-#include <pcl/point_cloud.h>
-#include <pcl_ros/point_cloud.h>
-#include <std_msgs/Header.h>
 #include <sensor_msgs/PointCloud2.h>
-#include <nav_msgs/Odometry.h>
-#include <pcl/common/transform.h>
-#include <pcl/filters/approximate_voxel_grid.h>
-
-#include <tf/tf.h>
-
-#include "Eigen/Core"
-#include "Eigen/Dense"
-#include "Eigen/LU"
-
-#ifdef _OPENMP
-#include <omp.h>
-#endif
-
-
 pc_callback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
     sensor_msgs::PointCloud2ConstPtr pc;
