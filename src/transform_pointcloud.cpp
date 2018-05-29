284985af4ae771eaedc1f9d4a132c64c1886f711
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-05-29 14:10:35 +0900

    update

--- src/transform_pointcloud.cpp
+++ src/transform_pointcloud.cpp
@@ -18,6 +18,11 @@ source_frame -> target_frame
 
 using namespace std;
 
+string INPUT_TOPIC;
+string OUTPUT_TOPIC;
+string TARGET_FRAME;
+string SOURCE_FRAME;
+
 ros::Time t;
 
 string target_frame = "/centerlaser";
@@ -34,13 +39,20 @@ int main(int argc, char** argv)
 {
     ros::init(argc, argv, "sq_transform");
     ros::NodeHandle n;
+
+    n.getParam("/transform_pointcloud/input_topic", INPUT_TOPIC);
+    n.getParam("/transform_pointcloud/output_topic", OUTPUT_TOPIC);
+    n.getParam("/transform_pointcloud/target_frame", TARGET_FRAME);
+    n.getParam("/transform_pointcloud/source_frame", SOURCE_FRAME);
     
     tf::TransformListener listener;
     tf::StampedTransform transform;
+
+    ros::Subscriber sub = n.subscribe(INPUT_TOPIC, 10, Callback);
+    ros::Publisher  pub = n.advertise<sensor_msgs::PointCloud2>(OUTPUT_TOPIC, 10);
 	
-	ros::Subscriber sub = n.subscribe("/sq_lidar/points/lcl", 10, Callback);
-    // ros::Subscriber sub = n.subscribe("/sq_lidar/points", 10, Callback);
-    ros::Publisher  pub = n.advertise<sensor_msgs::PointCloud2>("/sq_lidar/points/tf", 10);
+	// ros::Subscriber sub = n.subscribe("/sq_lidar/points/lcl", 10, Callback);
+    // ros::Publisher  pub = n.advertise<sensor_msgs::PointCloud2>("/sq_lidar/points/tf", 10);
 
     ros::Rate rate(60);
 
@@ -49,8 +61,8 @@ int main(int argc, char** argv)
         sensor_msgs::PointCloud pc_trans;
         sensor_msgs::PointCloud2 pc2_trans;
         try{
-            listener.waitForTransform(target_frame.c_str(), source_frame.c_str(), t, ros::Duration(1.0));
-            listener.transformPointCloud(target_frame.c_str(), t, pc_, source_frame.c_str(), pc_trans);
+            listener.waitForTransform(TARGET_FRAME.c_str(), SOURCE_FRAME.c_str(), t, ros::Duration(1.0));
+            listener.transformPointCloud(TARGET_FRAME.c_str(), t, pc_, SOURCE_FRAME.c_str(), pc_trans);
             sensor_msgs::convertPointCloudToPointCloud2(pc_trans, pc2_trans);
         }catch (tf::TransformException& ex) {
             ROS_WARN("[draw_frames] TF exception:\n%s", ex.what());
