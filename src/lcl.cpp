bb7b3dad822be4c5f20779b911cbb4f1b8db17be
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-06-01 15:45:21 +0900

    update

--- src/lcl.cpp
+++ src/lcl.cpp
@@ -1,10 +1,10 @@
 /*
-save points using odometry
+   save points using odometry
 
-Subscribe
-    
+   Subscribe
 
-Publish
+
+   Publish
 
 
 author : Yudai Sadakuni
@@ -61,28 +61,28 @@ float z_threshold = 30.0;
 
 Eigen::Matrix4f create_matrix(nav_msgs::Odometry odom_now, float reflect){
 
-	double roll_now, pitch_now, yaw_now;
-	
-	tf::Quaternion q_now(odom_now.pose.pose.orientation.x, odom_now.pose.pose.orientation.y, odom_now.pose.pose.orientation.z, odom_now.pose.pose.orientation.w);
-	tf::Matrix3x3(q_now).getRPY(roll_now, pitch_now, yaw_now);
-	
-	Eigen::Translation3f init_translation(reflect*odom_now.pose.pose.position.x, reflect*odom_now.pose.pose.position.y, reflect*odom_now.pose.pose.position.z);
-	Eigen::AngleAxisf init_rotation_x(reflect*roll_now, Eigen::Vector3f::UnitX());
-	Eigen::AngleAxisf init_rotation_y(reflect*pitch_now, Eigen::Vector3f::UnitY());
-	Eigen::AngleAxisf init_rotation_z(reflect*yaw_now, Eigen::Vector3f::UnitZ());
-	
-	Eigen::Matrix4f init_guess = (init_translation * init_rotation_z * init_rotation_y * init_rotation_x).matrix();
+    double roll_now, pitch_now, yaw_now;
 
-	return init_guess;
+    tf::Quaternion q_now(odom_now.pose.pose.orientation.x, odom_now.pose.pose.orientation.y, odom_now.pose.pose.orientation.z, odom_now.pose.pose.orientation.w);
+    tf::Matrix3x3(q_now).getRPY(roll_now, pitch_now, yaw_now);
+
+    Eigen::Translation3f init_translation(reflect*odom_now.pose.pose.position.x, reflect*odom_now.pose.pose.position.y, reflect*odom_now.pose.pose.position.z);
+    Eigen::AngleAxisf init_rotation_x(reflect*roll_now, Eigen::Vector3f::UnitX());
+    Eigen::AngleAxisf init_rotation_y(reflect*pitch_now, Eigen::Vector3f::UnitY());
+    Eigen::AngleAxisf init_rotation_z(reflect*yaw_now, Eigen::Vector3f::UnitZ());
+
+    Eigen::Matrix4f init_guess = (init_translation * init_rotation_z * init_rotation_y * init_rotation_x).matrix();
+
+    return init_guess;
 }
 
 void lcl_callback(nav_msgs::Odometry msg){
-	if(init_lcl_flag){
-		init_odom_ = msg;
-		init_lcl_flag = false;
-	}
-	odom_ = msg;
-	transform_matrix = create_matrix(odom_, 1.0);
+    if(init_lcl_flag){
+        init_odom_ = msg;
+        init_lcl_flag = false;
+    }
+    odom_ = msg;
+    transform_matrix = create_matrix(odom_, 1.0);
 }
 
 void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
@@ -99,20 +99,20 @@ void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
     CloudAPtr output_pc_after (new CloudA);
 
     for(size_t i=0;i<single_pc_->points.size();i++){
-		double distance = sqrt(pow(single_pc_->points[i].x, 2)+
-							   pow(single_pc_->points[i].y, 2)+
-							   pow(single_pc_->points[i].z, 2));
-		if(distance < 30){
-        //if(single_pc_->points[i].z <= z_threshold){
+        double distance = sqrt(pow(single_pc_->points[i].x, 2)+
+                pow(single_pc_->points[i].y, 2)+
+                pow(single_pc_->points[i].z, 2));
+        if(distance < 30){
+            //if(single_pc_->points[i].z <= z_threshold){
             PointA temp;
             output_pc_after->points.push_back(output_pc->points[i]);
         }
     }
 
-	CloudAPtr output_save_pc (new CloudA);
-	Eigen::Matrix4f inverse_transform_matrix = transform_matrix.inverse();
-	pcl::transformPointCloud(*output_pc_after, *output_save_pc, inverse_transform_matrix);
-	
+    CloudAPtr output_save_pc (new CloudA);
+    Eigen::Matrix4f inverse_transform_matrix = transform_matrix.inverse();
+    pcl::transformPointCloud(*output_pc_after, *output_save_pc, inverse_transform_matrix);
+
 
     if(count_ < save_num){
         *save_pc_ += *output_save_pc;
@@ -121,25 +121,23 @@ void pc_callback(const sensor_msgs::PointCloud2ConstPtr msg)
         int old_pc_size = (int)old_pc_->points.size();
         save_pc_->points.erase(save_pc_->points.begin(), save_pc_->points.begin()+old_pc_size);
         *save_pc_ += *output_save_pc;
-		old_pc_ = output_save_pc;
+        old_pc_ = output_save_pc;
     }
 
     sensor_msgs::PointCloud2 pc_;
-	pcl::toROSMsg(*save_pc_, pc_);
-	pc_.header.stamp = ros::Time::now();
+    pcl::toROSMsg(*save_pc_, pc_);
+    pc_.header.stamp = ros::Time::now();
     pc_.header.frame_id = msg->header.frame_id;
     pub.publish(pc_);
 
-	count_++;
-
+    count_++;
 }
-
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "lsl");
     ros::NodeHandle n;
-	n.getParam("lcl/save_num", save_num);
+    n.getParam("lcl/save_num", save_num);
     ros::Rate rate(20);
 
     ros::Subscriber sub_pc = n.subscribe("/cloud/tf", 30, pc_callback);
