b9ac1ae5427c860dcc5d357690388f734913d275
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-06-05 15:08:27 +0900

    update

--- calibration/ransac_lidar.cpp
+++ calibration/ransac_lidar.cpp
@@ -16,40 +16,14 @@
 #include <pcl/sample_consensus/model_types.h>
 #include <pcl/segmentation/sac_segmentation.h>
 
+#include <sensor_fusion/get_cluster_info.h>
+#include <sensor_fusion/plane_segmentation.h>
+
 typedef pcl::PointNormal PointA;
 typedef pcl::PointCloud<PointA> CloudA;
 typedef pcl::PointCloud<PointA>::Ptr CloudAPtr;
 
 ros::Publisher pub_plane;
-
-void plane_segmentation(CloudAPtr cloud, CloudA& plane);
-{
-    pcl::ModelCoefficients::Ptr coefficients (new pcl::ModelCoefficients);
-    pcl::PointIndices::Ptr inliers (new pcl::PointIndices);
-    // Create the segmentation object
-    pcl::SACSegmentation<pcl::PointXYZ> seg;
-    // Optional
-    seg.setOptimizeCoefficients (true);
-    // Mandatory
-    seg.setModelType (pcl::SACMODEL_PLANE);
-    seg.setMethodType (pcl::SAC_RANSAC);
-    seg.setDistanceThreshold (0.01);
-
-    seg.setInputCloud (cloud);
-    seg.segment (*inliers, *coefficients);
-
-    if (inliers->indices.size () == 0)
-    {
-        PCL_ERROR ("Could not estimate a planar model for the given dataset.");
-    }
-
-    // plane pointcloud
-    plane.points.resize(inliers->indices.size());
-    for(size_t i=0;i<inliers->indices.size();i++)
-    {
-        plane.points[i] = inliers->points[i];
-    }
-}
 
 void pcCallback(const sensor_msgs::PointCloud2ConstPtr& msg)
 {
