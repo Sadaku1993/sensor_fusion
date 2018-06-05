4708808707b11d1aa07a1163f7f2e9ef3f794695
Sadaku-Macbook <Sadaku1993@@users.noreply.github.com>
2018-06-05 15:57:07 +0900

    update

--- include/sensor_fusion/plane_segmentation.h
+++ include/sensor_fusion/plane_segmentation.h
@@ -38,8 +38,8 @@ void plane_segmentation(CloudAPtr cloud, CloudA& plane)
     // plane pointcloud
     for(size_t i=0;i<inliers->indices.size();i++)
     {
-        plane.points.resize(int(inliers->indices[i].indices.size()));
-        for(size_t j=0;j<inliers->indices[i].indices.size();j++){
+        plane.points.resize(inliers[i]->indices.size());
+        for(size_t j=0;j<inliers[i]->indices.size();j++){
             plane.points[i] = inliers->points[i].indices[j];
         }
     }
