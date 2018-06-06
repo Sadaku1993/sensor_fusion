a243023723dec50870d1bf91bbe50c926ba4d231
Sadaku1993 <Sadalu1993@users.noreply.github.com>
2018-06-07 04:12:13 +0900

    update

--- calibration/save_points.cpp
+++ calibration/save_points.cpp
@@ -4,26 +4,51 @@
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
-ros::Publisher pub;
+#define SAVE_NUM 100
+#define LIDAR_POINTS 500
+
+typedef pcl::PointXYZ PointA;
+typedef pcl::PointCloud<PointA> CloudA;
+typedef pcl::PointCloud<PointA>::Ptr CloudAPtr;
 
 using namespace std;
 
-int count = 0;
+ros::Publisher pub;
+
+int pc_count = 0;
+int t = 0;
+bool save = false;
+
+CloudA pcl_save;
 
 void Callback(const sensor_msgs::PointCloud2ConstPtr msg)
 {
     CloudAPtr cloud(new CloudA);
     pcl::fromROSMsg(*msg, *cloud);
-	
-	CloudA rm_ground;
-	constructFullClouds(cloud, rm_ground);
 
-	// Publish Coloured PointCloud
-	sensor_msgs::PointCloud2 output;
-    pcl::toROSMsg(rm_ground, output);
-    output.header.frame_id = msg->header.frame_id;
-    output.header.stamp = ros::Time::now();
-    pub.publish(output);
+    printf("size:%d\n", int(cloud->points.size()));
+    t = pc_count * int(cloud->points.size());
+
+    // for(size_t i=0;i<LIDAR_POINTS;i++)
+    // {
+    //     pcl_save.points[i+t] = cloud->points[i];
+    // }
+
+    // Publish Coloured PointCloud
+    if(save)
+    {
+        sensor_msgs::PointCloud2 output;
+        pcl::toROSMsg(pcl_save, output);
+        output.header.frame_id = msg->header.frame_id;
+        output.header.stamp = ros::Time::now();
+        pub.publish(output);
+    }
+
+    ++pc_count;
+    if(pc_count == SAVE_NUM){
+        save = true;
+        pc_count = 0;
+    }
 }
 
 int main(int argc, char**argv)
@@ -33,6 +58,8 @@ int main(int argc, char**argv)
 
     ros::Subscriber sub = n.subscribe("/cloud", 10, Callback);
     pub = n.advertise<sensor_msgs::PointCloud2>("/cloud/save", 10);
+
+    pcl_save.points.resize(SAVE_NUM*LIDAR_POINTS);
 	
     ros::spin();
 
