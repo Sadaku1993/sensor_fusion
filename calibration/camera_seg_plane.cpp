8040aea7640667bd984fbbbadb0d41f68c272a01
Sadaku1993 <Sadalu1993@users.noreply.github.com>
2018-06-14 17:03:02 +0900

    update

--- calibration/camera_seg_plane.cpp
+++ calibration/camera_seg_plane.cpp
@@ -5,11 +5,16 @@
 #include <iostream>
 
 #include <sensor_fusion/down_sampling.h>
+#include <sensor_fusion/plane_segmentation.h>
+#include <sensor_fusion/clustering.h>
+#include <sensor_fusion/outlier_removal.h>
 #include <sensor_fusion/pub_cloud.h>
 
 using namespace std;
 
 ros::Publisher pub_pickup;
+ros::Publisher pub_plane;
+ros::Publisher pub_outlier;
 
 string REALWORLD;
 double THRESHOLD_X;
@@ -36,8 +41,16 @@ void pcCallback(const sensor_msgs::PointCloud2ConstPtr msg)
 
     CloudAPtr pickup_cloud(new CloudA);
     pickup(cloud, pickup_cloud);
+    
+    CloudAPtr plane_cloud(new CloudA);
+    plane_segmentation(pickup_cloud, plane_cloud);
+
+    CloudAPtr outlier_cloud(new CloudA);
+    outlier_removal(plane_cloud, outlier_cloud);
 
     pub_cloud(pickup_cloud, msg->header, pub_pickup);
+    pub_cloud(plane_cloud, msg->header, pub_plane);
+    pub_cloud(outlier_cloud, msg->header, pub_outlier);
 }
 
 
@@ -51,7 +64,9 @@ int main(int argc, char**argv)
     n.getParam("threshold/z", THRESHOLD_Z);
 
     ros::Subscriber sub = n.subscribe("/cloud", 10, pcCallback);
-    pub_pickup   = n.advertise<sensor_msgs::PointCloud2>("/output/pickup", 10);
+    pub_pickup = n.advertise<sensor_msgs::PointCloud2>("/output/pickup", 10);
+    pub_plane  = n.advertise<sensor_msgs::PointCloud2>("/output/plane" , 10);
+    pub_outlier = n.advertise<sensor_msgs::PointCloud2>("/output/outlier", 10);
 
     ros::spin();
 
