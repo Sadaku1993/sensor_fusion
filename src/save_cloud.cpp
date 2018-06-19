1003441c86444cbf659e9d6c81ac493475ab2b2e
Sadaku1993 <ce172033@meiji.ac.jp>
2018-06-19 15:17:23 +0900

    update

--- src/save_cloud.cpp
+++ src/save_cloud.cpp
@@ -73,7 +73,7 @@ int main(int argc, char** argv)
     while(ros::ok())
     {
         listener.waitForTransform(TARGET_FRAME, SOURCE_FRAME, ros::Time(0), ros::Duration(1.0));
-        // PCDFileSaver(input_, listener, COUNT);
+        PCDFileSaver(input_, listener, COUNT);
         COUNT += 1;
     }
     return 0;
