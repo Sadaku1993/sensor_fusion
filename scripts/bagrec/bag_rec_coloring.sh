da63bebaa8288cfb1e5bed6bd2a39dd40f8e7318
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-06-22 09:04:00 +0900

    update

--- scripts/bagrec/bag_rec_coloring.sh
+++ scripts/bagrec/bag_rec_coloring.sh
@@ -11,9 +11,9 @@ TIME=$(date +%Y-%m-%d-%H-%M-%S)
 CLOUD="/cloud/tf"
 ODOM="/odom"
 IMU="/imu/data"
-ZED0="/zed0/left/image_rect_color/compressed /zed0/left/camera_info /zed0/ds_cloud"
-ZED1="/zed1/left/image_rect_color/compressed /zed1/left/camera_info /zed1/ds_cloud"
-ZED2="/zed2/left/image_rect_color/compressed /zed2/left/camera_info /zed2/ds_cloud"
+ZED0="/zed0/left/image_rect_color/compressed"
+ZED1="/zed1/left/image_rect_color/compressed"
+ZED2="/zed2/left/image_rect_color/compressed"
 COLORED="/sq_lidar/colored"
 TF="/tf /tf_static"
 
@@ -27,4 +27,4 @@ echo $ZED2 &
 echo $COLORED &
 echo $TF &
 
-/opt/ros/kinetic/bin/rosbag record $ODOM $IMU $COLORED -O /home/amsl/$TIME.bag
+/opt/ros/kinetic/bin/rosbag record $ODOM $IMU $COLORED $ZED0 $ZED1 $ZED2 -O /home/amsl/$TIME.bag
