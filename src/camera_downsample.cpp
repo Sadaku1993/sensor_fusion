7e3ddfdda7578adea194b1a8d8c21a1b9a6046dc
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-06-22 09:53:23 +0900

    update

--- src/camera_downsample.cpp
+++ src/camera_downsample.cpp
@@ -2,9 +2,15 @@
 #include <sensor_msgs/PointCloud2.h>
 #include <pcl_ros/point_cloud.h>
 #include <pcl/point_cloud.h>
+#include <pcl/io/pcd_io.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
 #include <iostream>
 
-#include <sensor_fusion/down_sampling.h>
+
+typedef pcl::PointXYZRGB PointA;
+typedef pcl::PointCloud<PointA> CloudA;
+typedef pcl::PointCloud<PointA>::Ptr CloudAPtr;
 #include <sensor_fusion/pub_cloud.h>
 
 using namespace std;
@@ -12,6 +18,14 @@ using namespace std;
 ros::Publisher pub_ds_cloud;
 
 double DS_SIZE = 0.03;
+void down_sampling(CloudAPtr cloud, CloudAPtr& cloud_filtered, float size)
+{
+    // Create the filtering object
+    pcl::VoxelGrid<PointA> sor;
+    sor.setInputCloud (cloud);
+    sor.setLeafSize (size, size, size);
+    sor.filter (*cloud_filtered);
+}
 
 void pcCallback(const sensor_msgs::PointCloud2ConstPtr msg)
 {
