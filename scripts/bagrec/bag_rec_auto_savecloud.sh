3fcc07660c70198fcf6b92e2bfae000252a196c6
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-07-13 07:47:08 +0900

    change bag data

--- scripts/bagrec/bag_rec_auto_savecloud.sh
+++ scripts/bagrec/bag_rec_auto_savecloud.sh
@@ -12,9 +12,11 @@ CLOUD="/cloud/tf"
 CLOUD_GLOBAL="/cloud/global"
 ODOM="/odom"
 IMU="/imu/data"
-ZED0="/zed0/left/image_rect_color/compressed /zed0/left/camera_info /zed0/move /zed0/depth/depth_registered/compressedDepth"
-ZED1="/zed1/left/image_rect_color/compressed /zed1/left/camera_info /zed1/move /zed1/depth/depth_registered/compressedDepth"
-ZED2="/zed2/left/image_rect_color/compressed /zed2/left/camera_info /zed2/move /zed2/depth/depth_registered/compressedDepth"
+ZED0="/zed0/left/image_rect_color/compressed /zed0/right/image_rect_color/compressed /zed0/left/camera_info  "
+ZED1="/zed1/left/image_rect_color/compressed /zed1/right/image_rect_color/compressed /zed1/left/camera_info  "
+ZED2="/zed2/left/image_rect_color/compressed /zed2/right/image_rect_color/compressed /zed2/left/camera_info  "
+DEPTH="/zed0/depth/depth_registered/compressedDepth /zed1/depth/depth_registered/compressedDepth /zed2/depth/depth_registered/compressedDepth"
+MOVE="/zed0/move /zed1/move /zed2/move"
 FLOW="/zed0/optical_flow/compressed /zed1/optical_flow/compressed /zed2/optical_flow/compressed"
 DIFF="/zed0/diff_image/compressed /zed1/diff_image/compressed /zed2/diff_image/compressed"
 NODE="/node"
@@ -30,10 +32,12 @@ echo $IMU &
 echo $ZED0 &
 echo $ZED1 &
 echo $ZED2 &
+echo $DEPTH &
+echo $MOVE &
 echo $FLOW &
 echo $DIFF &
 echo $NODE &
 echo $TRANSFORM &
 echo $TF &
 
-/opt/ros/kinetic/bin/rosbag record $CLOUD $CLOUD_GLOBAL $ODOM $IMU $ZED0 $ZED1 $ZED2 $DIFF $NODE $TRANSFORM -O /home/amsl/$TIME.bag
+/opt/ros/kinetic/bin/rosbag record $CLOUD $CLOUD_GLOBAL $ODOM $IMU $ZED0 $ZED1 $ZED2 $MOVE $DIFF $NODE $TRANSFORM -O /home/amsl/$TIME.bag
