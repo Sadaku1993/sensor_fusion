230acd027b2d78ecd0e31c1d7a3eaaf80cb57c61
Sadaku1993 <36195395+Sadaku1993@users.noreply.github.com>
2018-07-18 01:15:43 +0900

    update

--- scripts/bagrec/bag_rec_auto_savecloud.sh
+++ scripts/bagrec/bag_rec_auto_savecloud.sh
@@ -13,14 +13,14 @@ CLOUD_GLOBAL="/cloud/global"
 ODOM="/odom"
 IMU="/imu/data"
 
-ZED0="/zed0/left/image_rect_color/compressed /zed0/left/camera_info /zed0/depth/depth_registered"
-ZED1="/zed1/left/image_rect_color/compressed /zed1/left/camera_info /zed1/depth/depth_registered"
-ZED2="/zed2/left/image_rect_color/compressed /zed2/left/camera_info /zed2/depth/depth_registered"
+# ZED0="/zed0/left/image_rect_color/compressed /zed0/left/camera_info /zed0/depth/depth_registered"
+# ZED1="/zed1/left/image_rect_color/compressed /zed1/left/camera_info /zed1/depth/depth_registered"
+# ZED2="/zed2/left/image_rect_color/compressed /zed2/left/camera_info /zed2/depth/depth_registered"
 
-#ZED0="/zed0/left/image_rect_color/compressed /zed0/right/image_rect_color/compressed /zed0/left/camera_info  "
-#ZED1="/zed1/left/image_rect_color/compressed /zed1/right/image_rect_color/compressed /zed1/left/camera_info  "
-#ZED2="/zed2/left/image_rect_color/compressed /zed2/right/image_rect_color/compressed /zed2/left/camera_info  "
-#DEPTH="/zed0/depth/depth_registered/compressedDepth /zed1/depth/depth_registered/compressedDepth /zed2/depth/depth_registered/compressedDepth"
+ZED0="/zed0/left/image_rect_color/compressed /zed0/right/image_rect_color/compressed /zed0/left/camera_info  "
+ZED1="/zed1/left/image_rect_color/compressed /zed1/right/image_rect_color/compressed /zed1/left/camera_info  "
+ZED2="/zed2/left/image_rect_color/compressed /zed2/right/image_rect_color/compressed /zed2/left/camera_info  "
+DEPTH="/zed0/depth/depth_registered/compressedDepth /zed1/depth/depth_registered/compressedDepth /zed2/depth/depth_registered/compressedDepth"
 
 MOVE="/zed0/move /zed1/move /zed2/move"
 FLOW="/zed0/optical_flow/compressed /zed1/optical_flow/compressed /zed2/optical_flow/compressed"
@@ -45,4 +45,4 @@ echo $NODE &
 echo $TRANSFORM &
 echo $TF &
 
-/opt/ros/kinetic/bin/rosbag record $CLOUD $CLOUD_GLOBAL $ODOM $IMU $ZED0 $ZED1 $ZED2 $MOVE $NODE $TRANSFORM -O /home/amsl/$TIME.bag
+/opt/ros/kinetic/bin/rosbag record $CLOUD $CLOUD_GLOBAL $ODOM $IMU $ZED0 $ZED1 $ZED2 $DEPTH $MOVE $NODE $TRANSFORM -O /home/amsl/$TIME.bag
